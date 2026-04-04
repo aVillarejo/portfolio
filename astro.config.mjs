c10be0b9716caa14e16c085f1d4bae9e8671e2b0
Alberto Villarejo <ing.albertovillarejo@gmail.com>
2026-04-04 16:04:29 -0600

    chore: setup libraries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Be Vietnam Pro",
+      cssVariable: "--font-be-vietnam-pro",
+      weights: [400, 500, 600, 700],
+      styles: ["normal"],
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Gabarito",
+      cssVariable: "--font-gabarito",
+      weights: [400, 500, 600, 700],
+      styles: ["normal"],
+    },
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
